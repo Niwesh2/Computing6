ba0a30bc1be3d7d52e451aea0de637207e4f9b54
Niwesh Dhital <np01cp4a210205@islingtoncollege.edu.np>
2024-03-13 09:36:19 +0545

    Add project files.

---
+++ Computing6/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Computing6.Models.Student;
+using Microsoft.EntityFrameworkCore;
+
+namespace Computing6.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Computing6/Models/Student/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Computing6.Models.Student
+{
+    public class Student
+    {
+        [Key]
+        public int LMUID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string FypTitle { get; set; }
+
+
+    }
+}
